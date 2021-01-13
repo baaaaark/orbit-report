5e89228f55e4c214600fa0b9b0e245d3e42200c0
Jason Oliver <jason@pop-os.localdomain>
2021-01-13 12:11:44 -0600

    final commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+//import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
+import { Satellite } from './satellite';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'orbit-report';
+  sourceList: Satellite[];
+  displayList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    this.displayList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+
+          let fetchedSatellites = data.satellites;
+          for (let x = 0; x < fetchedSatellites.length; x++) {
+            let satellite = new Satellite(fetchedSatellites[x].name, fetchedSatellites[x].type, fetchedSatellites[x].launchDate, fetchedSatellites[x].orbitType, fetchedSatellites[x].operational)
+            this.sourceList.push(satellite);
+          }
+          this.displayList = this.sourceList.slice(0);
+        }.bind(this));
+    }.bind(this));
+  }
+
+    search(searchTerm: string): void {
+      let matchingSatellites: Satellite[] = [];
+      searchTerm = searchTerm.toLowerCase();
+      for(let i=0; i < this.sourceList.length; i++) {
+        let name = this.sourceList[i].name.toLowerCase();
+        if (name.indexOf(searchTerm) >= 0) {
+            matchingSatellites.push(this.sourceList[i]);
+        }
+      }
+      // assign this.displayList to be the array of matching satellites
+      // this will cause Angular to re-make the table, but now only containing matches
+      this.displayList = matchingSatellites;
+  }
+}
